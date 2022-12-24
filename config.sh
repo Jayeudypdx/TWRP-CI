f5e0b9198e5bedb570c4565c6443bbb839baec2d
Jayeudypdx <109179392+Jayeudypdx@users.noreply.github.com>
2022-12-24 09:24:25 -0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,7 +3,7 @@
 # Device
 export TWRP_MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp"
 export TWRP_BRANCH="twrp-12.1"
-export DT_LINK="https://github.com/Jayeudypdx/android_device_samsung_gta8wifi-1 -b twrp-11"
+export DT_LINK="https://github.com/Jayeudypdx/android_device_samsung_gta8wifi-1.git -b twrp-11"
 
 export DEVICE="gta8wifi"
 export OEM="samsung"
